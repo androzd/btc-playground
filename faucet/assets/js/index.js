55518f943d301da798bbfaf9e4945e45cb41dc9d
andrei.drozd <andrei@drozd.by>
2020-07-07 17:21:00 +0300

    sendtoaddress faucet

--- faucet/assets/js/index.js
+++ faucet/assets/js/index.js
@@ -1,5 +1,28 @@
+(function ($) {
+    $.fn.serializeFormJSON = function () {
+        var o = {};
+        var a = this.serializeArray();
+        var form = this;
+        $.each(a, function () {
+            if ('number' == $(form).find('[name='+this.name+']').attr('type')) {
+                this.value = parseFloat(this.value);
+            }
+            if (o[this.name]) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
+})(jQuery);
+
 $(document).on('click', '[data-ajax-href]', (event) => {
-    event.preventDefault();
+    event.preventDefault()
+    event.stopPropagation()
     let $target = $(event.currentTarget);
     let href = $target.data('ajax-href');
     let method = $target.data('ajax-method') || 'get';
@@ -9,6 +32,28 @@ $(document).on('click', '[data-ajax-href]', (event) => {
         method: method,
         success: (result) => {
             setBadge(result.status)
+        }
+    });
+})
+
+$(document).on('submit', '[data-ajax-form]', (event) => {
+    event.preventDefault()
+    let $form = $(event.currentTarget);
+    let href = $form.attr('action');
+    let method = $form.attr('method') || 'get';
+    let data = $form.serializeFormJSON();
+    console.log({url: href,
+        method: method,
+        contentType: "application/json",
+        dataType: "json",
+        data: data});
+    $.ajax({
+        url: href,
+        method: method,
+        contentType: "application/json",
+        data: JSON.stringify(data),
+        success: (result) => {
+            console.log(result)
         }
     });
 })
